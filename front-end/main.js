655faf4df99ea8d41aa392893e8ebf909c59cdf7
ewdemy <ewdemy@hotmail.com>
2021-03-29 11:57:22 -0300

    front-end estoque

--- front-end/main.js
+++ front-end/main.js
@@ -3,14 +3,20 @@ document.getElementById("item-estoque").click()
 function abrirMenu(){
     document.getElementById("menu").style.width = "250px"
     document.getElementById("container").style.marginLeft = "250px"
+    document.getElementById("btn-abrir").style.marginRight = "0px"
+    document.getElementById("foot").style.marginLeft = "250px"
 }
 
 function fecharMenu(){
     document.getElementById("menu").style.width = "0px"
     document.getElementById("container").style.marginLeft = "0px"
+    document.getElementById("btn-abrir").style.marginRight = "-250px"
+    document.getElementById("foot").style.marginLeft = "0px"
 }
 
 function abrirConteudo(event, idItem){
+    title = idItem.substring(0,1).toUpperCase().concat(idItem.substring(1))
+    document.title ="SGE - " + title
     var conteudos = document.getElementsByClassName("conteudo")
     var itensMenu = document.getElementsByClassName("item-menu")
 
@@ -22,7 +28,7 @@ function abrirConteudo(event, idItem){
         item.classList.remove("ativo")   
     }
 
-    document.getElementById(idItem).style.display = "block"
+    document.getElementById(idItem).style.display = "flex"
     event.target.classList.add("ativo")
     
 }
